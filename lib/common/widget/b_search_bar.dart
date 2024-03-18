4391759e8f65ebd02af3a75c29a5594aa920b0a5
NguyenLoi <hongloi123123@gmail.com>
2024-03-18 23:06:51 +0700

    Complete home screen

--- lib/common/widget/b_search_bar.dart
+++ lib/common/widget/b_search_bar.dart
@@ -1,27 +1,39 @@
 import 'package:budget_app/constants/icon_constants.dart';
 import 'package:flutter/material.dart';
 
-class BSearchBar extends StatelessWidget {
-  const BSearchBar({Key? key, required this.controller}) : super(key: key);
+class BSearchBar extends StatefulWidget {
+  const BSearchBar({Key? key, required this.controller, this.hintText})
+      : super(key: key);
   final TextEditingController controller;
+  final String? hintText;
 
+  @override
+  State<BSearchBar> createState() => _BSearchBarState();
+}
+
+class _BSearchBarState extends State<BSearchBar> {
   @override
   Widget build(BuildContext context) {
     return SearchBar(
-      controller: controller,
-      leading:  Icon(IconConstants.search),
+      controller: widget.controller,
+      hintText: widget.hintText,
+      onChanged: (value) {
+        setState(() {});
+      },
+      leading: Icon(IconConstants.search),
+      padding: const MaterialStatePropertyAll<EdgeInsets>(
+          EdgeInsets.symmetric(horizontal: 16.0)),
       trailing: <Widget>[
-        Tooltip(
-          message: 'Clear',
-          child: IconButton(
-            onPressed: () {
-              controller.clear();
-            },
-            icon: controller.text.isEmpty
-                ? const SizedBox.shrink()
-                : Icon(IconConstants.clear),
-          ),
-        )
+        widget.controller.text.isNotEmpty
+            ? IconButton(
+                isSelected: widget.controller.text.isNotEmpty,
+                onPressed: () {
+                  widget.controller.clear();
+                  setState(() {});
+                },
+                icon: Icon(IconConstants.clear),
+              )
+            : const SizedBox.shrink()
       ],
     );
   }
--- lib/constants/icon_constants.dart
+++ lib/constants/icon_constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconConstants {
   static IconData showPassword = Icons.visibility;
@@ -8,5 +9,8 @@ class IconConstants {
   static IconData search = Icons.search;
   static IconData clear = Icons.clear;
   static IconData arrowNext = Icons.arrow_forward_ios_rounded;
-
+  static IconData dropdown = Icons.keyboard_arrow_down;
+  static IconData emojiSmile = FontAwesomeIcons.faceSmile;
+  static IconData emojiSurprise = FontAwesomeIcons.faceSurprise;
+  static IconData emojiFrown = FontAwesomeIcons.faceFrown;
 }
