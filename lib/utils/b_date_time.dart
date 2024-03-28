1033c8b6b7bfba1eddb1d366fa22cb9aa2c66341
NguyenLoi <loi.nguyen.lifetechvn@gmail.com>
2024-03-28 15:25:11 +0700

    Complete deatail budget

--- lib/utils/b_date_time.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-class BDateTime {
-  static String toFormat(DateTime dateTime, {String strFormat = 'dd/MM/yyyy'}) {
-    return DateFormat(strFormat).format(dateTime);
-  }
-}
