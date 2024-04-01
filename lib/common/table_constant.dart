5588ffebba8da585afe2dbf0884ce066b4d036f6
NguyenLoi <loi.nguyen.lifetechvn@gmail.com>
2024-04-01 13:48:08 +0700

    Step 2

--- lib/common/table_constant.dart
+++
@@ -1,7 +0,0 @@
-class TableConstant {
-  static const String budget = 'Budget';
-  static const String budgetTransaction = 'BudgetTransaction';
-  static const String goal = 'Goal';
-  static const String goalTransaction = 'GoalTransaction';
-  static const String user = 'User';
-}
