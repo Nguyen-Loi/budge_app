6a76fc7e7286deb17b02fd57c7d3afd69ce69187
NguyenLoi <hongloi123123@gmail.com>
2024-05-22 07:50:07 +0700

    new model

---
+++ lib/models/merge_model/budget_transactions.dart
@@ -0,0 +1,34 @@
+import 'package:budget_app/core/extension/extension_data.dart';
+import 'package:budget_app/models/budget_model.dart';
+import 'package:budget_app/models/merge_model/transaction_card_model.dart';
+import 'package:budget_app/models/transaction_model.dart';
+
+class BudgetTransactionsModel{
+  final BudgetModel budget;
+  final List<TransactionModel> transactions;
+
+  BudgetTransactionsModel({
+    required this.budget,
+    required this.transactions,
+  });
+
+  List<BudgetTransactionsModel> toList(List<BudgetModel> budgets, List<TransactionModel> transactions){
+    List<BudgetTransactionsModel> list = [];
+    Map<String, List<TransactionModel>> transactionsMap = {};
+
+    for (var transaction in transactions) {
+      if (!transactionsMap.containsKey(transaction.budgetId)) {
+        transactionsMap[transaction.budgetId!] = [];
+      }
+      transactionsMap[transaction.budgetId]!.add(transaction);
+    }
+
+    for (var budget in budgets) {
+      final transactionsOfBudget = transactionsMap[budget.id] ?? [];
+      final model = BudgetTransactionsModel(budget: budget, transactions: transactionsOfBudget);
+      list.add(model);
+    }
+    return list;
+  }
+    
+}
