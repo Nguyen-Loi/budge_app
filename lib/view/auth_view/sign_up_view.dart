0127b439e50d2187897ec267be0c38678032bb5c
NguyenLoi <hongloi123123@gmail.com>
2024-06-03 06:05:51 +0700

    Remote warning app

--- lib/view/auth_view/sign_up_view.dart
+++ lib/view/auth_view/sign_up_view.dart
@@ -1,13 +1,11 @@
 import 'package:budget_app/common/widget/b_app_bar.dart';
 import 'package:budget_app/common/widget/b_text.dart';
 import 'package:budget_app/common/widget/button/b_button.dart';
-import 'package:budget_app/common/widget/form/b_form_checkbox.dart';
 import 'package:budget_app/common/widget/form/b_form_field_password.dart';
 import 'package:budget_app/common/widget/form/b_form_field_text.dart';
 import 'package:budget_app/constants/gap_constants.dart';
 import 'package:budget_app/core/extension/extension_validate.dart';
 import 'package:budget_app/localization/app_localizations_context.dart';
-import 'package:budget_app/theme/app_text_theme.dart';
 import 'package:budget_app/view/auth_view/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -91,26 +89,26 @@ class _LoginViewState extends ConsumerState<SignUpView> {
         ));
   }
 
-  Widget _terms() {
-    return BFormCheckbox(
-      validator: (p0) {
-        if (p0 == false) {
-          return context.loc.pleaseEnableService;
-        }
-        return null;
-      },
-      title: Text.rich(
-        TextSpan(
-          children: [
-            TextSpan(text: context.loc.nEableServiceDescription(0)),
-            TextSpan(
-                text: context.loc.nEableServiceDescription(1),
-                style: context.textTheme.bodyMedium)
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget _terms() {
+  //   return BFormCheckbox(
+  //     validator: (p0) {
+  //       if (p0 == false) {
+  //         return context.loc.pleaseEnableService;
+  //       }
+  //       return null;
+  //     },
+  //     title: Text.rich(
+  //       TextSpan(
+  //         children: [
+  //           TextSpan(text: context.loc.nEableServiceDescription(0)),
+  //           TextSpan(
+  //               text: context.loc.nEableServiceDescription(1),
+  //               style: context.textTheme.bodyMedium)
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _bEmailFormField() {
     return BFormFieldText(
