581e51fd6f3811f1f8764536355f23aa231a1cb7
NguyenLoi <loi.nguyen.lifetechvn@gmail.com>
2024-09-17 17:26:03 +0700

    format code report

--- lib/view/report_view/controller/report_controller.dart
+++ lib/view/report_view/controller/report_controller.dart
@@ -3,7 +3,7 @@ import 'package:budget_app/common/widget/dialog/b_dialog_info.dart';
 import 'package:budget_app/common/widget/dialog/b_loading.dart';
 import 'package:budget_app/core/b_excel.dart';
 import 'package:budget_app/core/enums/transaction_type_enum.dart';
-import 'package:budget_app/core/extension/extension_iterable.dart';
+import 'package:budget_app/core/extension/extension_datetime.dart';
 import 'package:budget_app/core/utils.dart';
 import 'package:budget_app/localization/app_localizations_context.dart';
 import 'package:budget_app/models/budget_model.dart';
@@ -13,26 +13,49 @@ import 'package:budget_app/models/models_widget/chart_budget_model.dart';
 import 'package:budget_app/models/user_model.dart';
 import 'package:budget_app/view/base_controller/budget_base_controller.dart';
 import 'package:budget_app/view/base_controller/transaction_base_controller.dart';
+import 'package:budget_app/view/report_view/components/report_filter_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final reportControllerProvider =
-    StateNotifierProvider.autoDispose<ReportController, DateTime>((ref) {
+    StateNotifierProvider.autoDispose<ReportController, ReportFilterModel>(
+        (ref) {
   final budgets = ref.watch(budgetBaseControllerProvider);
   final transactionsCard = ref.watch(transactionsBaseControllerProvider);
   return ReportController(budgets: budgets, transactionsCard: transactionsCard);
 });
 
-class ReportController extends StateNotifier<DateTime> {
+class ReportController extends StateNotifier<ReportFilterModel> {
   ReportController(
       {required List<BudgetModel> budgets,
       required List<TransactionCardModel> transactionsCard})
       : _budgets = budgets,
         _transactionsCard = transactionsCard,
-        super(DateTime.now()) {
-    _setRangeDateTime();
-    _reload();
+        super(ReportFilterModel(
+            dateTimeRange:
+                DateTimeRange(start: DateTime.now(), end: DateTime.now()),
+            transactionTypes: [
+              TransactionTypeEnum.incomeBudget,
+              TransactionTypeEnum.expenseBudget
+            ])) {
+    _init();
   }
+
+  void _init() {
+    final minDate = _budgets
+        .map((e) => e.startDate)
+        .reduce((a, b) => a.isBefore(b) ? a : b);
+    final maxDate =
+        _budgets.map((e) => e.endDate).reduce((a, b) => a.isAfter(b) ? a : b);
+
+    DateTimeRange dateTimeRangeInBudget =
+        DateTimeRange(start: minDate, end: maxDate);
+    _reportFilterValues =
+        _reportFilterValues.copyWith(dateTimeRange: dateTimeRangeInBudget);
+  }
+
+  late ReportFilterModel _reportFilterValues;
+  ReportFilterModel get reportFilterValues => _reportFilterValues;
 
   final List<BudgetModel> _budgets;
   final List<TransactionCardModel> _transactionsCard;
@@ -44,33 +67,34 @@ class ReportController extends StateNotifier<DateTime> {
       _budgetTransantionsList;
   List<ChartBudgetModel> get chartBudgetList => _chartBudgetCurrent;
 
-  late DateTimeRange _dateTimeRange;
-  DateTimeRange get datetimeRange => _dateTimeRange;
-
-  void _setRangeDateTime() {
+  void setDataFilter(ReportFilterModel filterModel) {
     if (_budgets.isEmpty) {
-      final now = DateTime.now();
-      _dateTimeRange = DateTimeRange(start: now, end: now);
       return;
     }
-    final minDate = _budgets
-        .map((e) => e.startDate)
-        .reduce((a, b) => a.isBefore(b) ? a : b);
-    final maxDate =
-        _budgets.map((e) => e.endDate).reduce((a, b) => a.isAfter(b) ? a : b);
-    _dateTimeRange = DateTimeRange(start: minDate, end: maxDate);
-  }
 
-  void updateDate(DateTime date) {
-    state = date;
-    _reload();
+    state = filterModel;
+
+    _chartBudgetCurrent = ChartBudgetModel.toList(
+        allTransactionCard: _transactionsCard
+            .where((e) =>
+                state.transactionTypes.contains(e.transactionType) &&
+                e.transaction.transactionDate
+                    .isBetweenDateTimeRange(state.dateTimeRange))
+            .toList());
+    final transactions = _transactionsCard
+        .map((e) => e.transaction)
+        .where((e) =>
+            e.transactionDate.isBetweenDateTimeRange(state.dateTimeRange))
+        .toList();
+    _budgetTransantionsList =
+        BudgetTransactionsModel.mapList(_budgets, transactions);
   }
 
   void exportExcel(BuildContext context, {required UserModel user}) async {
     final closeDialog = showLoading(context: context);
     final res = await BExcel.generatedReport(
       context,
-      dateTimeRange: _dateTimeRange,
+      dateTimeRange: state.dateTimeRange,
       list: _budgetTransantionsList,
     );
     closeDialog();
@@ -85,22 +109,5 @@ class ReportController extends StateNotifier<DateTime> {
         context,
       );
     });
-  }
-
-  void _reload() {
-    _chartBudgetCurrent = ChartBudgetModel.toList(
-        allTransactionCard: _transactionsCard
-            .where((e) =>
-                e.transaction.transactionType ==
-                TransactionTypeEnum.expenseBudget)
-            .filterByMonth(
-                time: state, getDate: (e) => e.transaction.transactionDate)
-            .toList());
-    final transactions = _transactionsCard
-        .map((e) => e.transaction)
-        .filterByMonth(time: state, getDate: (e) => e.transactionDate)
-        .toList();
-    _budgetTransantionsList =
-        BudgetTransactionsModel.mapList(_budgets, transactions);
   }
 }
