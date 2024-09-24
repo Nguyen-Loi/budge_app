9fb55abe201eb7a42b754c71addbc05d8842a4c6
NguyenLoi <loi.nguyen.lifetechvn@gmail.com>
2024-09-24 10:48:41 +0700

    Fix change language with provider

--- lib/common/shared_pref/language_controller.dart
+++ lib/common/shared_pref/language_controller.dart
@@ -1,9 +1,12 @@
 import 'package:budget_app/common/shared_pref/shared_utility_provider.dart';
 import 'package:budget_app/core/enums/language_enum.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-final languageControllerProvider = StateNotifierProvider<LanguageController, LanguageEnum>((ref) {
+
+final languageControllerProvider =
+    StateNotifierProvider<LanguageController, LanguageEnum>((ref) {
   return LanguageController(ref: ref);
 });
+
 class LanguageController extends StateNotifier<LanguageEnum> {
   LanguageController({required this.ref}) : super(LanguageEnum.vietnamese) {
     state =
