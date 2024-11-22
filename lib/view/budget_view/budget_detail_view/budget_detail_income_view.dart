b8b8bc98dfd8c5bc01a956bb52b67811583b7ad5
Nguyen Loi <hongloi123123@gmail.com>
2024-11-22 09:17:06 +0700

    improve the UI

--- lib/view/budget_view/budget_detail_view/budget_detail_income_view.dart
+++ lib/view/budget_view/budget_detail_view/budget_detail_income_view.dart
@@ -1,9 +1,7 @@
-import 'package:budget_app/common/widget/b_text.dart';
-import 'package:budget_app/constants/gap_constants.dart';
+import 'package:budget_app/constants/assets_constants.dart';
 import 'package:budget_app/core/extension/extension_money.dart';
 import 'package:budget_app/localization/app_localizations_context.dart';
 import 'package:budget_app/models/budget_model.dart';
-import 'package:budget_app/theme/app_text_theme.dart';
 import 'package:budget_app/view/budget_view/budget_detail_view/budget_base_detail_view.dart';
 import 'package:flutter/material.dart';
 
@@ -14,25 +12,14 @@ class BudgetDetailIncomeView extends BudgetBaseDetailView {
   @override
   List<Widget> header(BuildContext context, BudgetModel budget) {
     return [
-      _currentIncome(context),
-      gapH8,
-      BText.b3(strTime(context, model: budget)),
+      itemStatus(context),
+      itemRow(context,
+          svgAsset: SvgAssets.money,
+          label: context.loc.currentIncome,
+          value: budget.currentAmount.toMoneyStr(),
+          colorValue: Theme.of(context).colorScheme.tertiary),
+      itemOperatingTime(context),
+      itemReview(context)
     ];
-  }
-
-  Widget _currentIncome(BuildContext context) {
-    return RichText(
-        text: TextSpan(children: [
-      TextSpan(
-        text: '${context.loc.currentIncome}: ',
-        style: context.textTheme.bodyMedium!,
-      ),
-      TextSpan(
-        text: budget.currentAmount.toMoneyStr(),
-        style: context.textTheme.bodyLarge!.copyWith(
-            color: Theme.of(context).colorScheme.tertiary,
-            fontWeight: FontWeight.w700),
-      )
-    ]));
   }
 }
