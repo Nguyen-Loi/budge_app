10f5321be5f5f97a3fa4a3796e857ceab62e0bb7
LoiNguyen <loi.nguyen@lifetechvn.net>
2025-05-07 23:57:32 +0700

    support working with web

--- lib/constants/size_constants.dart
+++ lib/constants/size_constants.dart
@@ -1,3 +1,16 @@
-class SizeConstants{
+import 'package:flutter/material.dart';
+
+class SizeConstants {
   static const double maxWidthBase = 700;
+  static const double tablet = 700;
+  static const double desktop = 1200;
+
+  static bool isSmallScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < 600;
+  }
+
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 600 &&
+        MediaQuery.of(context).size.width < tablet;
+  }
 }
